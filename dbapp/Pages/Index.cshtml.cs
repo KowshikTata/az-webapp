2626790513babbfd52b17fc02f3375c69be672de
tatakowshik <Tata.Kowshik@ibm.com>
2022-08-27 22:14:32 +0530

    Add project files.

---
+++ dbapp/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using dbapp.Models;
+using dbapp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dbapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public List<Product> Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+
+        }
+    }
+}
